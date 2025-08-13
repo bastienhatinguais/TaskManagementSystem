4177efa8ce9736212b6db69d8d2c8899a272c767
Hatinguais Bastien <bastien.hatinguais@gmail.com>
2025-08-13 12:00:20 -0400

    Added Task class and database migration

---
+++ TaskManagementSystem.Api/Entities/Task.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementSystem.Api.Entities;
+
+public class Task
+{
+    public Guid Id { get; set; }
+
+    public required string Title { get; set; }
+    
+    public string Description { get; set; } = string.Empty;
+
+    public DateTime DueDate { get; set; }
+
+    public bool IsCompleted { get; set; } = false;
+}
---
+++ TaskManagementSystem.Api/Migrations/20250813155729_AddTaskMigration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagementSystem.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
