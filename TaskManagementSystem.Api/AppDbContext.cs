754ab890c2a85469b149c6e2edcf6467b442aa76
Hatinguais Bastien <bastien.hatinguais@gmail.com>
2025-08-13 11:59:54 -0400

    Added support for Entity Framework Core

---
+++ TaskManagementSystem.Api/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManagementSystem.Api;
+
+using Microsoft.EntityFrameworkCore;
+using TaskManagementSystem.Api.Entities;
+
+public class AppDbContext(DbContextOptions<AppDbContext> opts) : DbContext(opts)
+{
+    public DbSet<Task> Tasks => Set<Task>();
+}
