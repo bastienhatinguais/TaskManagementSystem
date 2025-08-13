f10a36497454e580520263cab4f912a2133af86f
Hatinguais Bastien <bastien.hatinguais@gmail.com>
2025-08-13 12:40:13 -0400

    Add TaskService.

---
+++ TaskManagementSystem.Api/Services/Interfaces/ITaskService.cs
@@ -0,0 +1,11 @@
+﻿using TaskManagementSystem.Api.Entities;
+
+namespace TaskManagementSystem.Api.Services.Interfaces;
+
+public interface ITaskService
+{
+    Task<ToDoTask> AddTaskAsync(ToDoTask task);
+    Task<List<ToDoTask>> GetAllTasksAsync();
+    Task<ToDoTask?> GetTaskByIdAsync(int id);
+    ToDoTask UpdateTask(ToDoTask task);
+}
---
+++ TaskManagementSystem.Api/Services/TaskService.cs
@@ -0,0 +1,15 @@
+﻿using TaskManagementSystem.Api.Entities;
+using TaskManagementSystem.Api.Repositories.Interfaces;
+using TaskManagementSystem.Api.Services.Interfaces;
+
+namespace TaskManagementSystem.Api.Services;
+public class TaskService(ITaskRepository taskRepository) : ITaskService
+{
+    public Task<ToDoTask?> GetTaskByIdAsync(int id) => taskRepository.GetByIdAsync(id);
+
+    public Task<List<ToDoTask>> GetAllTasksAsync() => taskRepository.GetAllAsync();
+
+    public Task<ToDoTask> AddTaskAsync(ToDoTask task) => taskRepository.AddAsync(task);
+
+    public ToDoTask UpdateTask(ToDoTask task) => taskRepository.Update(task);
+}
