816ddf12ef75c2ab5640fd4c507be8d4d1fa3ddf
Hatinguais Bastien <bastien.hatinguais@gmail.com>
2025-08-13 17:06:21 -0400

    Add new shared project containing DTOs

---
+++ TaskManagementSystem.Shared/DTOs/TaskDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace TaskManagementSystem.Shared.DTOs;
+
+public class TaskDto
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public DateTime DueDate { get; set; }
+
+    public bool IsCompleted { get; set; }
+}
