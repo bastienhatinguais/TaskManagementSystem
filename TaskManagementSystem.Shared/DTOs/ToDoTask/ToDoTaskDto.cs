336011771fa661d1f67d42776d5d80524f78f114
Hatinguais Bastien <bastien.hatinguais@gmail.com>
2025-08-15 14:33:18 -0400

    Refactor ToDoTaskDto to inherit from ToDoTaskUpsertDto

--- TaskManagementSystem.Shared/DTOs/ToDoTask/ToDoTaskDto.cs
+++ TaskManagementSystem.Shared/DTOs/ToDoTask/ToDoTaskDto.cs
@@ -1,15 +1,7 @@
 ﻿using System;
 namespace TaskManagementSystem.Shared.DTOs.ToDoTask;
 
-public class ToDoTaskDto
+public class ToDoTaskDto : ToDoTaskUpsertDto
 {
     public Guid Id { get; set; }
-
-    public string Title { get; set; } = string.Empty;
-
-    public string Description { get; set; } = string.Empty;
-
-    public DateTime DueDate { get; set; }
-
-    public bool IsCompleted { get; set; }
 }
