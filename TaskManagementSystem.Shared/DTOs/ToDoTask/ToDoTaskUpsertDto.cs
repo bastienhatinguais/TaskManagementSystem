69a9509d77157c58d7d70810150e77f2ab305547
Hatinguais Bastien <bastien.hatinguais@gmail.com>
2025-08-15 14:52:23 -0400

    Add validators on upsert task

--- TaskManagementSystem.Shared/DTOs/ToDoTask/ToDoTaskUpsertDto.cs
+++ TaskManagementSystem.Shared/DTOs/ToDoTask/ToDoTaskUpsertDto.cs
@@ -1,11 +1,16 @@
-﻿namespace TaskManagementSystem.Shared.DTOs.ToDoTask;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementSystem.Shared.DTOs.ToDoTask;
 
 public class ToDoTaskUpsertDto
 {
+    [Required, StringLength(140)]
     public string Title { get; set; } = string.Empty;
 
-    public string Description { get; set; } = string.Empty;
+    [StringLength(1000)]
+    public string? Description { get; set; }
 
+    [Required, DataType(DataType.Date)]
     public DateTime DueDate { get; set; }
 
     public bool IsCompleted { get; set; }
