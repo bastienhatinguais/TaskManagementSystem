16bc1fada418f029cfde00f305cd3b54fd99d35b
Hatinguais Bastien <bastien.hatinguais@gmail.com>
2025-08-16 15:49:57 -0400

    Add appsettings file for client side

--- TaskManagementSystem.Client/Program.cs
+++ TaskManagementSystem.Client/Program.cs
@@ -1,13 +1,15 @@
 using TaskManagementSystem.Client;
+using TaskManagementSystem.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using TaskManagementSystem.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7069/") });
+var apiBase = builder.Configuration["ApiBaseUrl"] ?? builder.HostEnvironment.BaseAddress;
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(apiBase) });
+
 builder.Services.AddScoped<ITaskService, TaskService>();
 
 await builder.Build().RunAsync();
