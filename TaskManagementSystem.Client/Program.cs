088e8c8f93272f80caefa0496d05f421cb96cd96
Hatinguais Bastien <bastien.hatinguais@gmail.com>
2025-08-18 09:46:53 -0400

    Cleanup unused library

--- TaskManagementSystem.Client/Program.cs
+++ TaskManagementSystem.Client/Program.cs
@@ -3,7 +3,6 @@ using TaskManagementSystem.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TaskManagementSystem.Client.Handler;
-using TailBlazor.Toast;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
