289582e9a659e1747009ce06abdb61a6bdccc4ca
Hatinguais Bastien <bastien.hatinguais@gmail.com>
2025-08-18 09:47:25 -0400

    Add restrictions to the fields of the task entity

--- TaskManagementSystem.Api/Entities/ToDoTask.cs
+++ TaskManagementSystem.Api/Entities/ToDoTask.cs
@@ -6,10 +6,13 @@ public class ToDoTask
 {
     public Guid Id { get; set; }
 
+    [StringLength(140)]
     public required string Title { get; set; }
-    
+
+    [StringLength(1000)]
     public string Description { get; set; } = string.Empty;
 
+    [DataType(DataType.Date)]
     public DateTime DueDate { get; set; }
 
     public bool IsCompleted { get; set; } = false;
